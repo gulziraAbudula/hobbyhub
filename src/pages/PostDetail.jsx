711c6b940d1c772487eb6eeb7b14ddc1ddf33275
Gulzira Abdullah <gzabudula21@gmail.com>
2026-07-27 15:36:46 -0400

    create post detail page

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,7 +1,61 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { supabase } from "../client"
+import "./PostDetail.css"
+
 export const PostDetail = () => {
+
+    const { id } = useParams()
+    const [post, setPost] = useState()
+
+    useEffect (() => {
+        fetchPost()
+    }, [])
+
+    const fetchPost = async () => {
+        const { data, error } = await supabase
+                            .from("Posts")
+                            .select("*")
+                            .eq("id", id)
+                            .single()
+        if (error) {
+            console.log(error)
+            return
+        }
+
+        setPost(data)
+    }
+
+    if(!post) {
+        return <h2>Loading...</h2>
+    }
+
     return (
         <>
-            <h1>Post Detail</h1>
+            <div className="post-detail">
+                <h1>{post.title}</h1>
+                <p>
+                    Created:
+                    {" "}
+                    {post.created_at
+                        ? new Date(post.created_at).toLocaleDateString()
+                        : "No date"}
+                </p>
+
+                <p>
+                    👍 {post.upvotes}
+                </p>
+
+                <p>{post.content}</p>
+
+                {post.image_url && (
+                    <img
+                        src={post.image_url}
+                        alt={post.title}
+                        width="500"
+                    />
+                )}
+            </div>
         </>
     )
 }
